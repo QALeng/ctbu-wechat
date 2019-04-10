1370ffb1d245d238d95485381aacc4ed33bdf012
unknown <unknown@example.com>
2019-04-10 21:20:05 +0800

    bookSearch的重传
    之前的bookSearch不是满意版本，所以将之前的替换上去了

--- pages/core/booksearch/booksearch.js
+++ pages/core/booksearch/booksearch.js
@@ -4,35 +4,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    searchContent:"",//搜索内容
-    total:[],//返回的总的数据
-    feedBackData:"",//如果返回的数据为空或者访问失败给出的提示
-    corename:['all',"Book","Journal"],//搜索类型
+    searchContent: "",//搜索内容
+    total: [],//返回的总的数据
+    feedBackData: "",//如果返回的数据为空或者访问失败给出的提示
+    corename: ['all', "Book", "Journal"],//搜索类型
     // 全部，图书，期刊，万方
     // type1:0,
-    searchModel:['front','seg','contain','exact'],//搜索模式
+    searchModel: ['front', 'seg', 'contain', 'exact'],//搜索模式
     // 前方搜索，分词检索，包含，绝对一致
     // model:0,
-    field: ['title', 'abstract', 'creatorSearch', 'publisher', 'isbnSearch', 'callNumberSearch', 'subject','all'],//搜索范围
+    field: ['title', 'abstract', 'creatorSearch', 'publisher', 'isbnSearch', 'callNumberSearch', 'subject', 'all'],//搜索范围
     // 题名，摘要，责任者，出版者，标准编码，索书号，主题词，全部字段
     // field1:0,
-    disk:['no','yes'],//随书光盘
+    disk: ['no', 'yes'],//随书光盘
     // disk1:0,
-    array:[0,0,0,0],
+    array: [0, 0, 0, 0],
     //对应corename,searchModel,field,disk
-    pages:0,//总共的页数
-    nowPage:"",
-    nowClass:"",
-    nextText:"",//下一页
-    lastText:"",//上一页
-    lastClass:"",//上一页的样式
-    nextClass:"",
-    pageNumber:"",//当前的页数
+    pages: 0,//总共的页数
+    nowPage: "",
+    nowClass: "",
+    nextText: "",//下一页
+    lastText: "",//上一页
+    lastClass: "",//上一页的样式
+    nextClass: "",
+    pageNumber: "",//当前的页数
     inputPage: "",//页面跳转输入框的内容
-    totalPages:"",//页面跳转输入框的样式
+    totalPages: "",//页面跳转输入框的样式
     confirm: "",//页面跳转确认
-    confirmContent:"",
-    toPage:""//输入跳转的页数
+    confirmContent: "",
+    toPage: ""//输入跳转的页数
   },
 
   /**
@@ -45,49 +45,49 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
   //输入
   searchInput: function (e) {
@@ -98,24 +98,24 @@ Page({
     console.log(e.detail.value);
   },
   // 用于美化初始页面
-  bottomChange:function(lastText,lastClass,nextText,nextClass,nowText,nowClass){
-    this.setData({lastText:lastText});
-    this.setData({nextText:nextText});
-    this.setData({lastClass:lastClass});
-    this.setData({nextClass:nextClass});
-    this.setData({nowPage:nowText});
-    this.setData({nowClass:nowClass});
+  bottomChange: function (lastText, lastClass, nextText, nextClass, nowText, nowClass) {
+    this.setData({ lastText: lastText });
+    this.setData({ nextText: nextText });
+    this.setData({ lastClass: lastClass });
+    this.setData({ nextClass: nextClass });
+    this.setData({ nowPage: nowText });
+    this.setData({ nowClass: nowClass });
   },//接到bottomChange
-  secondChange:function(judge){
-    if(judge){
+  secondChange: function (judge) {
+    if (judge) {
       this.setData({ pageNumber: 1 });
       this.setData({ inputPage: 'inputPage' });
       this.setData({ confirm: "confirm" });
       this.setData({ confirmContent: "确认" });
       var pages = this.data.pages;
       this.setData({ totalPages: "请输入页数  " + "总页数为：" + pages });
-    }else{
-      this.setData({ pageNumber: ''});
+    } else {
+      this.setData({ pageNumber: '' });
       this.setData({ inputPage: '' });
       this.setData({ confirm: "" });
       this.setData({ confirmContent: "" });
@@ -128,23 +128,23 @@ Page({
     console.log('sucess');
   },
   // 获取返回值
-    manySetData:function(total,judge,pages){
-    if(total.length==0){
-      if (judge == 1) 
+  manySetData: function (total, judge, pages) {
+    if (total.length == 0) {
+      if (judge == 1)
         this.setData({ feedBackData: "无该字眼的书或者访问失败" });
-      this.bottomChange("","","","","","");
+      this.bottomChange("", "", "", "", "", "");
       this.secondChange(0);
     }
     else
-      this.setData({feedBackData:""})
-      this.setData({total:total});
-      this.setData({pages:pages});
+      this.setData({ feedBackData: "" })
+    this.setData({ total: total });
+    this.setData({ pages: pages });
     console.log('sucess');
   },
   //搜索按钮
-  searchButton:function(){
-    var judge='Null';//用于处理异常情况
-    var a=this.data.array;
+  searchButton: function () {
+    var judge = 'Null';//用于处理异常情况
+    var a = this.data.array;
     var type1 = a[0];//搜索类型
     var searchType = this.data.corename;
     var model = a[1];//搜索模式
@@ -154,16 +154,16 @@ Page({
     var disk1 = a[3];//随书光盘
     var searchDisk = this.data.disk;
     var total;
-    var that=this.manySetData;
-    var changeClass=this.bottomChange;
-    var secondChange=this.secondChange;
-    var keyWord=this.data.searchContent;//搜索的关键字
+    var that = this.manySetData;
+    var changeClass = this.bottomChange;
+    var secondChange = this.secondChange;
+    var keyWord = this.data.searchContent;//搜索的关键字
     var filed;//搜索范围
     var searchModel;//搜索的模式
     var theSame;//地址相同的部分
     var pages;//页数
-    var thisPoint=this;
-    if (keyWord.length!=0){
+    var thisPoint = this;
+    if (keyWord.length != 0) {
       this.setData({ feedBackData: "" });
       var requestUrl = getApp().totalUrl.bookSearchUrl + 'search/' + keyWord + '/' + searchModel[model] + '/' + searchType[type1] + '/' + searchField[field] + '/' + searchDisk[disk1];
       console.log(requestUrl);
@@ -172,8 +172,8 @@ Page({
         success: function (res) {
           total = res.data.searchResult;
           console.log(res.data.data1);
-          judge=res.data.judge;
-          try{
+          judge = res.data.judge;
+          try {
             if (judge == "True") {
               pages = res.data.pages;
               console.log(pages);
@@ -183,14 +183,15 @@ Page({
               console.log('next');
             } else {
               throw 'Null';
-            }}catch(error){
-              judge=error;
-          }finally{
-            if(judge=="Null"){
+            }
+          } catch (error) {
+            judge = error;
+          } finally {
+            if (judge == "Null") {
               console.log('lllll');
               changeClass("", "", "", "", "", '');
               total = [];
-              that(total, 1,1);
+              that(total, 1, 1);
               thisPoint.setData({ judge: 'Null' });
             }
           }
@@ -201,50 +202,49 @@ Page({
           // return;
         }
       })
-    }else{
-      this.setData({ feedBackData:"请输入关键字"});
+    } else {
+      this.setData({ feedBackData: "请输入关键字" });
     }
   },
   //设置搜索按钮
-  setSearch:function(){
-    var a=this.data.array;
-    var setUrl = '../setSearch/setSearch?type1='+a[0]+'&model='+a[1]+'&field='+a[2]+'&disk='+a[3];
-    console.log(setUrl);    
+  setSearch: function () {
+    var a = this.data.array;
+    var setUrl = '../setSearch/setSearch?type1=' + a[0] + '&model=' + a[1] + '&field=' + a[2] + '&disk=' + a[3];
+    console.log(setUrl);
     wx.navigateTo({
       url: setUrl
     })
   },
   // 切换到下一页
-  nextPage:function(){
-    var pagesNumber=this.data.pages;
-    var nowNumber=this.data.pageNumber;
-    var nN=parseInt(nowNumber);
+  nextPage: function () {
+    var pagesNumber = this.data.pages;
+    var nowNumber = this.data.pageNumber;
+    var nN = parseInt(nowNumber);
     var nextUrl;
-    var that=this.totalSetData;
-    var total=[];
-    if(nN<pagesNumber)
-    {
+    var that = this.totalSetData;
+    var total = [];
+    if (nN < pagesNumber) {
       nN = nN + 1;
       var passPage = nN - 1;
-      var str=''+passPage;
+      var str = '' + passPage;
       // nextUrl = "http://127.0.0.1:5000/changePage/"+str;
-      nextUrl = getApp().totalUrl.bookSearchUrl+"changePage/" + str;      
+      nextUrl = getApp().totalUrl.bookSearchUrl + "changePage/" + str;
       wx.request({
         url: nextUrl,
-        success:function(res){
+        success: function (res) {
           total = res.data.searchResult;
           that(total);
-          return ;
+          return;
         }
       })
     }
     else
-      nN=pagesNumber;
+      nN = pagesNumber;
     console.log(nN);
-    this.setData({pageNumber:nN});
+    this.setData({ pageNumber: nN });
   },
   // 切换到上一页
-   lastPage: function () {
+  lastPage: function () {
     var pagesNumber = this.data.pages + 1;
     console.log(pagesNumber);
     var nowNumber = this.data.pageNumber;
@@ -252,12 +252,11 @@ Page({
     var that = this.totalSetData;
     var total = [];
     var nN = parseInt(nowNumber);
-    if (nN>1)
-    {
-      nN = nN-1;
-      var passPage=nN-1;
+    if (nN > 1) {
+      nN = nN - 1;
+      var passPage = nN - 1;
       var str = '' + passPage;
-      lastUrl = getApp().totalUrl.bookSearchUrl+"changePage/" + str;
+      lastUrl = getApp().totalUrl.bookSearchUrl + "changePage/" + str;
       wx.request({
         url: lastUrl,
         success: function (res) {
@@ -273,20 +272,19 @@ Page({
     this.setData({ pageNumber: nN });
   },
   // 跳转确认按钮
-  confirmButton:function(){
-    var toPage=this.data.toPage;
+  confirmButton: function () {
+    var toPage = this.data.toPage;
     console.log(toPage);
-    var tP=parseInt(toPage);
-    var str = getApp().totalUrl.bookSearchUrl+"changePage/";
-    var maxPage=this.data.pages+1;
+    var tP = parseInt(toPage);
+    var str = getApp().totalUrl.bookSearchUrl + "changePage/";
+    var maxPage = this.data.pages + 1;
     var that = this.totalSetData;
     var total = [];
     console.log('comfirm');
     console.log(tP);
-    if (tP>0&&tP<=maxPage)
-    {
-      this.setData({ pageNumber:tP});
-      var passPage = tP-1;
+    if (tP > 0 && tP <= maxPage) {
+      this.setData({ pageNumber: tP });
+      var passPage = tP - 1;
       str = str + passPage;
       wx.request({
         url: str,
@@ -300,9 +298,10 @@ Page({
   },
   copyButton: function (res) {
     var numberText = res.target.dataset.text;
-    // console.log(numberText);
+
+    console.log(numberText);
     wx.setClipboardData({
       data: numberText,
     })
-    }
+  }
 })
